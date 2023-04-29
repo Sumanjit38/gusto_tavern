b86e20a2bbd63ca01a5d2b8a479c454f8f2ca9df
Sumanjit38 <sumanjit38@gmail.com>
2023-04-30 03:39:32 +0530

    update aboutus

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -15,8 +15,19 @@ const AboutUs = () => (
         <img src={images.spoon} alt='about_spoon' className='spoon__img' />
         <p className='p_opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra adipiscing ultrices vulputate posuere tristique. In sed odio nec aliquet eu proin mauris et.</p>
         <button type='button' className= 'custom__button'>Know More</button>
-
       </div>
+
+      <div className='app__aboutus-content_knife flex__center'>
+        <img src={images.knife} alt='about_knife' />
+      </div>
+
+      <div className='app__aboutus-content_history'>
+        <h1 className='headtext__cormorant'>Our History</h1>
+        <img src={images.spoon} alt='about_spoon' className='spoon__img' />
+        <p className='p_opensans'>Adipiscing tempus ullamcorper lobortis odio tellus arcu volutpat. Risus placerat morbi volutpat habitasse interdum mi aliquam In sed odio nec aliquet.</p>
+        <button type='button' className= 'custom__button'>Know More</button>
+      </div>
+  
     </div>
   </div>
 );
